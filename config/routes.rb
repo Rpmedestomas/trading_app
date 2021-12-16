d5cefaa7e45288427d2f05cc5f45d3b6742dd67a
Arwie Mendoza <arwie@avionschool.com>
2021-12-16 20:48:48 +0800

    Testing stuff

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
   resources :stocks
-  root 'users_devise_sessions_new'
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
