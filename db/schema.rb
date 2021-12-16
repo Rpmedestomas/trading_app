e62fe5557f63a6c01dacf36dbc8c0240a395884c
Arwie Mendoza <arwie@avionschool.com>
2021-12-16 20:03:10 +0800

    First commit

--- db/schema.rb
+++ db/schema.rb
@@ -15,13 +15,6 @@ ActiveRecord::Schema.define(version: 2021_12_16_025540) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "articles", force: :cascade do |t|
-    t.string "name"
-    t.text "body"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "stocks", force: :cascade do |t|
     t.string "name"
     t.float "price"
