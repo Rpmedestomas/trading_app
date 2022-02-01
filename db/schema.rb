fa7efc6e6826868f6ccbb4063542ce8a53af148d
Rpmedestomas <rpmedestomas@gmail.com>
2022-02-01 21:16:06 +0800

    Fixed manifest error

--- db/schema.rb
+++ db/schema.rb
@@ -44,13 +44,12 @@ ActiveRecord::Schema.define(version: 2021_12_24_022307) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.boolean "admin", default: false
-    t.float "money", default: 0.0
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.boolean "admin", default: false
+    t.float "money", default: 0.0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
